f0c08cfe0a98ad0986217eb122167851bd1cb304
TaoMaru <jackson_maria1@columbusstate.edu>
2023-10-13 20:52:00 -0400

    Add test cases for app and error not found

---
+++ src/test/App.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders app less than 50', () => {
+  render(<App count={25}/>);
+  const linkElement = screen.getByText(/Navbar/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders app at 50', () => {
+  render(<App count={50}/>);
+  const linkElement = screen.getByText(/Pricing/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders app greater than 50', () => {
+  render(<App count={55}/>);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
