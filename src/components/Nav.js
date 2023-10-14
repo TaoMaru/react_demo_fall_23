3ed972f7721b9b3531834b08cfae8872a46e6436
TaoMaru <jackson_maria1@columbusstate.edu>
2023-10-13 21:57:04 -0400

    Add cypress e2e testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="/home">Home</a>
               <a className="nav-link" href="#">Features</a>
               <a className="nav-link" href="#">Pricing</a>
               <a className="nav-link disabled" aria-disabled="true">Disabled</a>
